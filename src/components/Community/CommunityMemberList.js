16dd91132a10b321bbb19176f819c38713256616
Raksha <shetty.raksha@gmail.com>
2022-01-28 21:08:23 +0530

    member list display correctly

--- src/components/Community/CommunityMemberList.js
+++ src/components/Community/CommunityMemberList.js
@@ -3,93 +3,37 @@ import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import './index.css';
 import { List, Avatar, Button, Skeleton } from 'antd';
-import {useState} from 'react';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 const count = 3;
-const fakeDataUrl = `https://randomuser.me/api/?results=${count}&inc=name,gender,email,nat,picture&noinfo`;
 
- export default function CommunityMemberList{
-     
-const [initLoading, setLoading] =useState(true)
-    const [loading, setLoading] = useState(false)
-    const [data, setData] = useState([])
-    const [list, setList]= useState([])
+export default function CommunityMemberList() {
 
+   const [list, setList] = useState([])
 
-    fetch(fakeDataUrl)
-      .then(res => res.json())
-      .then(res => {
-        setLoading(false)
-         setData(res.results)
-          setList(res.results)
-    
-      });
-  }
+  useEffect(() => {
+    axios.get(`https://jsonplaceholder.typicode.com/users`)
+      .then((res) => { setList(res.data) })
+  }, []);
 
-  onLoadMore = () => {
-  
-        setLoading(true)
-      setList(setData(data.concat(
-        [...new Array(count)].map(() => ({ loading: true, name: {}, picture: {} })))))
-  
-    fetch(fakeDataUrl)
-      .then(res => res.json())
-      .then(res => {
-        setData(data.concat(res.results))
-        
-            setList(data)
-            setLoading(false)
-        
-         
-            // Resetting window's offsetTop so as to display react-virtualized demo underfloor.
-            // In real scene, you can using public method of react-virtualized:
-            // https://stackoverflow.com/questions/46700726/how-to-use-public-method-updateposition-of-react-virtualized
-            window.dispatchEvent(new Event('resize'));
-          }
-        );
-      
-  
+  console.log(list);
 
-  render() {
-    const { initLoading, loading, list } = this.state;
-    const loadMore =
-      !initLoading && !loading ? (
-        <div
-          style={{
-            textAlign: 'center',
-            marginTop: 12,
-            height: 32,
-            lineHeight: '32px',
-          }}
-        >
-          <Button onClick={this.onLoadMore}>loading more</Button>
-        </div>
-      ) : null;
-
-    return (
-      <List
-        className="demo-loadmore-list"
-        loading={initLoading}
+  return (
+    <div className="flex-center max-w-7xl mx-auto px-4 lg:pt-24">
+        <List
         itemLayout="horizontal"
-        loadMore={loadMore}
         dataSource={list}
         renderItem={item => (
           <List.Item
-            actions={[<a key="list-loadmore-edit">edit</a>, <a key="list-loadmore-more">more</a>]}
+            actions={[<a key="list-loadmore-edit">Remove</a>, <a key="list-loadmore-more">Edit</a>, <a key="list-loadmore-more">Chat</a>]}
           >
-            <Skeleton avatar title={false} loading={item.loading} active>
-              <List.Item.Meta
-                avatar={<Avatar src={item.picture.large} />}
-                title={<a href="https://ant.design">{item.name.last}</a>}
-                description="Ant Design, a design language for background applications, is refined by Ant UED Team"
-              />
-              <div>content</div>
-            </Skeleton>
+            <List.Item.Meta
+              title={item.name}
+            />
           </List.Item>
         )}
       />
-    );
-  }
-}
+      </div>
+  );
 
-ReactDOM.render(<CommunityMemberList />, document.getElementById('container'));
+}
