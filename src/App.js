57cb4ddc664a26c6525e174bdb4ab5dd4208fc1f
Vikram Kumar <vikramraimumbai@gmail.com>
2022-02-01 03:15:19 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Landing from 'pages/Landing';
 import Profile from 'pages/Profile';
@@ -23,30 +22,6 @@ function App() {
             <Redirect from="*" to="/" />
         </Switch>
     );
-=======
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
->>>>>>> main
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import App from 'App';
+import reportWebVitals from 'reportWebVitals';
 
 ReactDOM.render(
-<<<<<<< HEAD
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-=======
     <React.StrictMode>
         <BrowserRouter>
         
@@ -18,7 +12,6 @@ ReactDOM.render(
         </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
->>>>>>> himanshu
 );
 
 // If you want to start measuring performance in your app, pass a function
