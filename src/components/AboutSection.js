52c5d9cee6566983b5b17b3890edd3574d7a919c
himanshu-r <himanshureddy3@gmail.com>
2022-01-31 08:57:12 +0530

    About Us Section CHnages

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import Image from "@material-tailwind/react/Image";
 import H3 from '@material-tailwind/react/Heading3';
-import AboutImg from '../assets/img/teamwork.jpeg'
+import AboutImg from '../assets/img/teamwork.jpeg';
+import Paragraph from '@material-tailwind/react/Paragraph';
 
 function AboutSection() {
   return <div>
 
-      <div className='flex'>
+      <div className='flex' style={{"padding":" 100px 0px"}}>
 
-          <div className='md:w-6/12'> 
+          <div className='md:w-6/12' style={{"margin-right":"50px"}}> 
               <Image src={AboutImg} />
           </div>
 
-          <div className='space-x-2 md:w-6/12 '>
+          <div className=' md:w-6/12 ' style={{"margin":"50px"}}>
               <H3>This is the title of About Us </H3>
-              <p>This is the Description of About us Section</p>
+
+              <Paragraph>This is the Description of About us Section</Paragraph>
           </div>
 
       </div>
